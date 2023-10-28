42ffde7211763db09d1249c41d38fc57c7b84f72
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-28 17:51:49 +0500

    Changed A Bit Of Settings And Model

--- main/models.py
+++ main/models.py
@@ -8,6 +8,6 @@ class Todo(models.Model):
     deadline = models.DateTimeField(null=True, blank=True)
     media = models.FileField(upload_to="../media_storage", null=True, blank=True)
     subtask_of = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    task_by = models.ForeignKey(User, related_name="task_by")
+    task_by = models.ForeignKey(User, related_name="task_by", on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
