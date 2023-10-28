f144d7f80368d08764eb2e08a721d9ea2aba5c82
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-28 17:17:34 +0500

    ToDo List Plan Django

---
+++ main/plan.py
@@ -0,0 +1,27 @@
+# A Todo App 
+
+"""
+
+# Features:
+
+- Accounts
+    - Registration
+    - Login    
+    - Edit Profile
+    - Logout
+
+- Todo List 
+    - Create
+    - Update
+    - Display
+    - Delete
+    - Complete
+    ToDo
+        - Task
+        - Description
+        - Deadline
+        - SubTask Of (Task)
+        - Task Of (User)
+        - Photo Of Task
+        
+"""
