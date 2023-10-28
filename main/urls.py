81a9b76b7dd7101f22e735be3be84c1d597a76ce
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-28 17:19:05 +0500

    Updated ToDo List Urls

--- main/urls.py
+++ main/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path("login", views.login_view, name = "login"),
     path("todos", views.todos_view, name = "todos"),
     path("create_todo", views.create_todo_view, name = "create_todo"),
-    path("update_todo", views.update_todo_view, name = "update_todo"),
-    path("delete_todo", views.delete_todo_view, name = "delete_todo"),
-    path("complete_todo", views.complete_todo_view, name = "complete_todo"),
-    path("user_profile", views.user_profile_view, name = "user_profile"),
-    path("update_user_profile", views.update_user_profile_view, name = "update_user_profile"),
-    path("logout_user_profile", views.logout_user_profile_view, name = "logout_user_profile")
+    path("update_todo/<str:primary_key>", views.update_todo_view, name = "update_todo"),
+    path("delete_todo/<str:primary_key>", views.delete_todo_view, name = "delete_todo"),
+    path("complete_todo/<str:primary_key>", views.complete_todo_view, name = "complete_todo"),
+    path("user_profile/<str:primary_key>", views.user_profile_view, name = "user_profile"),
+    path("update_user_profile/<str:primary_key>", views.update_user_profile_view, name = "update_user_profile"),
+    path("logout_user_profile/<str:primary_key>", views.logout_user_profile_view, name = "logout_user_profile")
 ]
